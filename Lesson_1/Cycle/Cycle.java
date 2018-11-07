86b9284fe1d05b7bbb309ca8bd258a0fd9859e36
Kirill Slepenkov <kirill2595@gmail.com>
2018-11-07 21:38:49 +0500

    implement HW_1

--- Lesson_1/Cycle/Cycle.java
+++ Lesson_1/Cycle/Cycle.java
@@ -1,15 +1,16 @@
 public class Cycle {
     public static void main(String[] args) {
         for (int i = 0; i <= 20; i++) {
-            System.out.println(i);
+            System.out.print(" " + i);
         }
         System.out.println(" ");
 
         int a = 6;
         
         while (a >= -6) {
-            System.out.println(a);
+            System.out.print(" " + a);
             a = a - 2;
         }
+        System.out.println(" ");
     }
 }
