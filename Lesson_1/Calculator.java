080877f87e38c55e4955b9694b57ae3164b696b4
Kirill Slepenkov <kirill2595@gmail.com>
2018-12-03 21:11:29 +0500

    implement HW_1

---
+++ Lesson_1/Calculator.java
@@ -0,0 +1,43 @@
+public class Calculator {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        double sum = 0;
+        sum = a + b;
+        if (a <= b) {
+            System.out.println(a + " + " + b + " = " + sum);
+        } else {
+            System.out.println(b + " + " + a + " = " + sum);
+        }
+        sum = a - b;
+        if (a <= b) {
+            System.out.println(a + " - " + b + " = " + sum);
+        } else {
+            System.out.println(b + " - " + a + " = " + sum);
+        }
+        sum = a * b;
+        if (a <= b) {
+            System.out.println(a + " * " + b + " = " + sum);
+        } else {
+            System.out.println(b + " * " + a + " = " + sum);
+        }
+        sum = a / b;
+        if (a <= b) {
+            System.out.println(a + " / " + b + " = " + sum);
+        } else {
+            System.out.println(b + " / " + a + " = " + sum);
+        }
+        sum = a % b;
+        if (a <= b) {
+            System.out.println(a + " % " + b + " = " + sum);
+        } else {
+            System.out.println(b + " % " + a + " = " + sum);
+        }
+        sum = a ^ b;
+        if (a <= b) {
+            System.out.println(a + " ^ " + b + " = " + sum);
+        } else {
+            System.out.println(b + " ^ " + a + " = " + sum);
+        }
+    }
+}
