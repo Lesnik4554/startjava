4f859504fc2e32a25b27059931ea520f5925a737
Kirill Slepenkov <kirill2595@gmail.com>
2018-12-09 19:12:34 +0500

    implement HW_1

--- Lesson_1/Calculator.java
+++ Lesson_1/Calculator.java
@@ -2,9 +2,10 @@ public class Calculator {
     public static void main(String[] args) {
         int a = 1;
         int b = 2;
+        char sign = '+';
         double sum = 0;
         sum = a + b;
-        if (true) {
+        if (sign == '+') {
             System.out.println(a + " + " + b + " = " + sum);
         } else {
             System.out.println(b + " + " + a + " = " + sum);
